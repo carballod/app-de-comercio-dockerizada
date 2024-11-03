310f4a15f4f9062f6a703000b34f2234daca8637
Daniela Carballo <daniela.carballo@biggertech.co>
2024-11-02 22:09:10 -0300

    Add mongo config

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,16 @@
 import { configureExpress } from "./infrastructure/config/express.config";
+import { connectToDatabase } from "./infrastructure/config/mongodb.config";
 
 const app = configureExpress();
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+connectToDatabase()
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.error("Failed to connect to MongoDB", err);
+    process.exit(1);
+  });
